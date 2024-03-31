f4c6ce1d3cdf73cead09824a84109317e586b4e5
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2024-03-31 15:25:53 +0530

    Update First_St_App.py

--- First_St_App.py
+++ First_St_App.py
@@ -117,9 +117,9 @@ st.write("Completed")
 # Usage of multiselect widget
 
 options_1 = st.multiselect(
-    'What are your favorite colors, citys',
+    'What are your favorite colors and citys',
     ['Green', 'Yellow', 'Red', 'Blue'],
-    ['Yellow', 'Red'],
- ['Bagalkot','Bengaluru','Belagavi','Ballari','Bidar','Raichur'])
+    ['Yellow', 'Red'])
+    #['Bagalkot','Bengaluru','Belagavi','Ballari','Bidar','Raichur'])
 
 st.write('You selected:', options_1)
